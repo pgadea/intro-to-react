9e44ef7eda50868e7fc88d5de896d771206904c6
Pierre <pgadea7@gmail.com>
2017-08-08 22:50:17 -0500

    intro to react

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const NavBar = () =>
+<ul>
+  <li>Home</li>
+  <li>About</li>
+  <li>Contact</li>
+  <li>Blog</li>
+</ul>
+
+const HelloWorld = (props) => <div className="title">Hello {props.title}</div>
+
+const About = (props) => {
+  const postDate = new Date()
+
+  return (
+    <div>
+    <p>{'Posted:'}</p>
+    <p>{postDate.toString()}</p>
+    </div>
+  )
+}
+
+const Section = (props) => {
+  const name = '-' + props.name;
+
+  return(
+  <div className="post-card">
+    <p>{props.content}</p>
+    <p>{props.name}</p>
+    <p>{props.children}</p>
+  </div>
+  )
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+         <HelloWorld title = {'Drogo'} />
+         <Section content = {'Worldbreaker'} name={'Hulk'}>
+           <About />
+         </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
